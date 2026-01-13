352dfdd3ac0c2fe1a5b9f69cbc2591e5720ab41d
Kaci Hamroun <kacihamrounpro@gmail.com>
2026-01-13 16:48:45 +0100

    favicon & topIcon changes

--- src/components/TopIcon.js
+++ src/components/TopIcon.js
@@ -2,7 +2,7 @@
 
     const TopIcon = ({iconSource }) => {
         return (
-            <div className="cursor-pointer flex justify-center items-center w-full h-full bg-white rounded-full shadow-md">
+            <div className="cursor-pointer flex justify-center items-center w-full h-full bg-white rounded-full">
                 <img src={iconSource} alt="Scroll to top" className='w-full h-full'  />
             </div>
         );
